359aca35ab3cbffcb5d864d71d4a825197919368
spinillac11 <92007521+spinillac11@users.noreply.github.com>
2024-04-02 03:56:23 +0000

    Primera implementacion del hueco en el contenedor

--- particle.h
+++ particle.h
@@ -5,15 +5,17 @@ struct Particle{
     //Posiciones de la particula
     double x = 0;
     double y = 0;
-    //Particula valida dentro de la simulacion
-    //bool valid = true;
 
+    //Verificar particula dentro de los limites
+    bool out = false; 
+
+    //Mover la particula verificando que no salga de los limites 
     void move(double xmax, double ymax){
         int direction = rand() % 4;
         switch(direction){
             //Abajo
             case 0:
-                if (y > 0){
+                if (y > 1){
                     y -= 1;
                 }else{
                     y += 1;
@@ -22,7 +24,7 @@ struct Particle{
 
             //Arriba
             case 1:
-                if (y < ymax){
+                if (y < ymax-1){
                     y += 1;
                 }else{
                     y -= 1;
@@ -31,7 +33,7 @@ struct Particle{
 
             //Izquierda
             case 2:
-                if (x > 0){
+                if (x > 1){
                     x -= 1;
                 }else{
                     x += 1;
@@ -40,7 +42,7 @@ struct Particle{
 
             //Derecha
             case 3:
-                if (x < xmax){
+                if (x < xmax-1){
                     x += 1;
                 }else{
                     x -= 1;
@@ -49,4 +51,56 @@ struct Particle{
         }
     }
 
+    //Mover la particula con un agujero en el contenedor
+    void move_leak(double xmax, double ymax){
+        // Verificar si la partícula ya salió
+        if (out) {
+            return;
+        }
+
+        int direction = rand() % 4;
+        switch(direction){
+            //Abajo
+            case 0:
+                
+                if (y > 1 && (x < 30 || x > 70)) { 
+                    y -= 1;
+                    // Verificar si la partícula ha alcanzado la parte inferior del hueco
+                    if (y < 0) {
+                        out = true;
+                    }
+                } else {
+                    y += 1;
+                }
+                break;
+
+            //Arriba
+            case 1:
+                if (y < ymax-1){
+                    y += 1;
+                }else{
+                    y -= 1;
+                }
+                break;
+
+            //Izquierda
+            case 2:
+                if (x > 1){
+                    x -= 1;
+                }else{
+                    x += 1;
+                }
+                break;
+
+            //Derecha
+            case 3:
+                if (x < xmax-1){
+                    x += 1;
+                }else{
+                    x -= 1;
+                }
+                break;
+        }
+    }
 };
+
